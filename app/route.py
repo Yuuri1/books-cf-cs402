cc9a4a83fc5383051bd5889cb4f300a7982033d7
gandalfrr <112260333+gandalfrr@users.noreply.github.com>
2022-10-25 20:48:00 +0800

    Update route.py

--- app/route.py
+++ app/route.py
@@ -18,8 +18,8 @@ def index():
 
 @app.route('/register',methods=['GET','POST'])
 def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
+    #if current_user.is_authenticated:
+        #return redirect(url_for('home'))
     form=RegistrationForm()
     if form.validate_on_submit():
         max_id = db.session.query(db.func.max(User.user_id)).scalar()
@@ -31,8 +31,8 @@ def register():
     return render_template('register.html',title='Register',form=form)
 @app.route('/login',methods=['GET','POST'])
 def login():
-    if current_user.is_authenticated:    
-        return redirect(url_for('home'))
+    #if current_user.is_authenticated:    
+        #return redirect(url_for('home'))
     form=LoginForm()
     if form.validate_on_submit():
         user=User.query.filter_by(email=form.email.data).first()
@@ -48,7 +48,28 @@ def login():
 @app.route('/home')
 @login_required
 def home():
-    return render_template('home.html', title='Home')
+    print("User:::::",current_user.user_id)
+    form=updateEmail()
+
+    available=False
+    fileRating = os.path.join(app.static_folder, 'ratings.json')
+    fileBook=os.path.join(app.static_folder, 'books.json')
+        
+    with open(fileRating) as f:
+            dataRating = json.load(f)
+    for i in range(len(dataRating)):
+        if dataRating[i]["user_id"]==current_user.user_id:
+            print(dataRating[i]["user_id"])
+            available=True
+        if available==False:
+            suggested=[{"book_id":0,"original_title":"No rating, data unavailable","genre":"-"}]
+            rated=[{"book_id":0,"original_title":"No rating, data unavailable","genre":"-"}]
+    if available==True:
+        predict,rated=runRecommender(fileBook,fileRating,current_user.user_id)
+        suggested=json.loads(predict)
+        rated=json.loads(rated)
+        return render_template('home.html', title='Home',form=form,suggested=suggested,rated=rated)
+    #return render_template('home.html', title='Home')
 
 @app.route('/account',methods=['GET','POST'])
 @login_required
@@ -58,6 +79,7 @@ def account():
     if request.method == 'POST':
         if form.validate_on_submit():
             current_user.email = form.email.data
+            current_user.password=form.password.data
             db.session.commit()
             flash('Your account has been updated!', 'success')
             return redirect(url_for('account'))
@@ -84,8 +106,7 @@ def account():
             suggested=json.loads(predict)
             rated=json.loads(rated)
         return render_template('account.html', title='Account',form=form,suggested=suggested,rated=rated)
-
-    return render_template('account.html', title='Account',form=form)
+    #return render_template('account.html', title='Account',form=form)
 
 @app.route('/recommend',methods=['GET','POST'])
 @login_required
@@ -93,7 +114,10 @@ def recommend():
     book=""
     rating=""
     form=RatingForm()
-
+    
+    if request.method == 'GET':
+            with open('/booksrecommendation/booksrecommender/app/static/books.json', 'r', encoding="utf8") as blog_file:
+                book = json.load(blog_file)
     if request.method == 'POST':
         print(form.book_id.data)
         if form.validate_on_submit():
@@ -117,17 +141,10 @@ def recommend():
             return redirect(url_for('recommend'))
         else:
             flash('Rating Unsuccessful','danger')
-    return render_template('recommend.html',book=book,form=form, title='Recommend')
+        
+    return render_template('recommend.html',book=book,form=form, title='Rate')
 
 
-@app.route('/book',methods=['GET','POST'])
-@login_required
-def book():
-    if request.method == 'GET':
-        filename = os.path.join(app.static_folder, 'books.json')
-        with open(filename) as blog_file:
-            book = json.load(blog_file)
-    return render_template('book.html',book=book, title='Book')
 @app.route('/about')
 @login_required
 def about():
@@ -138,6 +155,7 @@ def about():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
 
 
 
